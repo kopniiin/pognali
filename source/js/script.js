7bf4eaaf83725ae72b292ce3fd9efc1a275e52ae
kopniiin <kopniiin@gmail.com>
2020-01-12 17:16:08 +0300

    Добавляет блок фильтрации пользователей

--- source/js/script.js
+++ source/js/script.js
@@ -1,8 +1,6 @@
-"user strict";
-
-let navigation = document.querySelector(".navigation");
-let navigationControl = navigation.querySelector(".navigation__control");
-let navigationLogo = navigation.querySelector(".navigation__logo");
+var navigation = document.querySelector(".navigation");
+var navigationControl = navigation.querySelector(".navigation__control");
+var navigationLogo = navigation.querySelector(".navigation__logo");
 
 navigation.classList.remove("navigation_no-js");
 
@@ -10,3 +8,27 @@ navigationControl.addEventListener("click", () => {
   navigation.classList.toggle("navigation_open");
   navigationLogo.classList.toggle("logo_blue");
 });
+
+var userFilter = document.querySelector(".user-filter");
+
+if (userFilter) {
+  var checkboxFilters = document.querySelectorAll(".checkbox-filter");
+  for (var checkboxFilter of checkboxFilters) {
+    var checkboxFilterControl = checkboxFilter.querySelector(".checkbox-filter__control");
+    handleFilter(checkboxFilter, checkboxFilterControl, "checkbox-filter_open");
+  }
+
+  var transportFilter = document.querySelector(".transport-filter");
+  var transportFilterControl = transportFilter.querySelector(".transport-filter__control");
+  handleFilter(transportFilter, transportFilterControl, "transport-filter_open");
+
+  var levelFilter = document.querySelector(".level-filter");
+  var levelFilterControl = levelFilter.querySelector(".level-filter__control");
+  handleFilter(levelFilter, levelFilterControl, "level-filter_open");
+}
+
+function handleFilter(filter, control, classToOpen) {
+  control.addEventListener("click", () => {
+    filter.classList.toggle(classToOpen);
+  });
+}
