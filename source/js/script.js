2f3c2acd75599c3c12cad6c72e58106dd9b517c6
kopniiin <kopniiin@gmail.com>
2020-01-13 20:53:30 +0300

    Добавляет блок модального окна и улучшает навигацию

--- source/js/script.js
+++ source/js/script.js
@@ -1,13 +1,59 @@
 var navigation = document.querySelector(".navigation");
-var navigationControl = navigation.querySelector(".navigation__control");
-var navigationLogo = navigation.querySelector(".navigation__logo");
 
-navigation.classList.remove("navigation_no-js");
+if (navigation) {
+  var navigationControl = navigation.querySelector(".navigation__control");
+  var navigationLogo = navigation.querySelector(".navigation__logo");
+  var navigationLinks = navigation.querySelectorAll(".navigation-link");
 
-navigationControl.addEventListener("click", () => {
-  navigation.classList.toggle("navigation_open");
-  navigationLogo.classList.toggle("logo_blue");
-});
+  navigation.classList.remove("navigation_no-js");
+
+  navigationControl.addEventListener("click", function() {
+    navigation.classList.toggle("navigation_open");
+
+    if (!navigation.classList.contains("navigation_scroll")) {
+      navigationLogo.classList.toggle("logo_blue");
+    }
+  });
+
+  window.addEventListener("scroll", function() {
+    if (window.pageYOffset >= 100 && !navigation.classList.contains("navigation_scroll")) {
+      navigation.classList.add("navigation_scroll");
+
+      navigationLogo.classList.add("logo_blue");
+
+      for (var link of navigationLinks) {
+        link.classList.add("navigation-link_dark");
+      }
+    } else if ( window.pageYOffset < 100 && navigation.classList.contains("navigation_scroll")) {
+      navigation.classList.remove("navigation_scroll");
+
+      navigationLogo.classList.remove("logo_blue");
+
+      for (var link of navigationLinks) {
+        link.classList.remove("navigation-link_dark");
+      }
+    }
+  });
+}
+
+var modalButton = document.querySelector(".modal-button");
+
+if (modalButton) {
+  var modal = document.querySelector(".modal");
+  var modalCloseButton = modal.querySelector(".modal__close-button");
+
+  modalButton.addEventListener("click", function(evt) {
+    evt.preventDefault();
+
+    modal.classList.add("modal_open");
+  });
+
+  modalCloseButton.addEventListener("click", function(evt) {
+    evt.preventDefault();
+
+    modal.classList.remove("modal_open");
+  });
+}
 
 var userFilter = document.querySelector(".user-filter");
 
@@ -20,7 +66,9 @@ if (userFilter) {
 }
 
 function handleFilter(filter, filterControl) {
-  filterControl.addEventListener("click", () => {
+  filterControl.addEventListener("click", function(evt) {
+    evt.preventDefault();
+
     filter.classList.toggle("filter_open");
   });
 }
